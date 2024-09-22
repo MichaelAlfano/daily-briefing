bc16e617d648ca9c5bf5c14c19a9b5aa6ec97143
Michael Alfano <michaelalfano2004@gmail.com>
2024-09-21 17:13:39 -0700

    Update token_util.py

--- token_util.py
+++ token_util.py
@@ -7,7 +7,5 @@ def get_plaid_access_token():
     return os.getenv('PLAID_ACCESS_TOKEN')
 
 def set_plaid_access_token(token):
-    # In a real-world scenario, you'd store this token in a more secure place
-    # like a database or a secure environment variable system
-    os.environ['PLAID_ACCESS_TOKEN'] = token
-    # You should also update the .env file or your secure storage with the new token
+    # TODO
+    return None
